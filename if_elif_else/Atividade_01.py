8b826f1242759b7fe0da15731bbaee7e3f115af4
LuDesigner <luis.designer.oficial@gmail.com>
2025-09-08 11:39:58 -0300

    Adicionando pastas e arquivos do projeto

--- if_elif_else/Atividade_01.py
+++ if_elif_else/Atividade_01.py
@@ -1,13 +1,44 @@
-"""
+import os
+import sys
 
-Bruno gerencia um pequeno comércio e quer saber qual produto teve o melhor desempenho de vendas no mês passado. Ele registrou a quantidade vendida de dois produtos: maçãs e bananas. Agora, ele precisa escrever um programa que identifique e exiba qual deles teve maior venda.
+def atividade(): 
+    print("""
+    Bruno gerencia um pequeno comércio e quer saber qual produto teve o melhor desempenho de vendas no mês passado. 
+    Ele registrou a quantidade vendida de dois produtos: maçãs e bananas. 
+    Agora, ele precisa escrever um programa que identifique e exiba qual deles teve maior venda.
 
-Crie um programa que receba o número de vendas dos dois produtos e exiba uma mensagem indicando qual deles vendeu mais. Se as quantidades forem iguais, exiba uma mensagem dizendo que houve empate.
+    Crie um programa que receba o número de vendas dos dois produtos e exiba uma mensagem indicando qual deles vendeu mais. 
+    Se as quantidades forem iguais, exiba uma mensagem dizendo que houve empate.
 
-Saída esperada:
+    """)
 
+def reinicializar():
+    print("""
+          \n
+    -------------------------------------------------
+    Vamos reiniciar o programa para uma nova análise?
+    -------------------------------------------------
+    """)
+    reinicializar = input("Digite 's' para sim ou 'n' para não: ").lower()
+    if reinicializar == 's':
+        os.system('cls' if os.name == 'nt' else 'clear')
+        main()
+    else:
+        print("Programa encerrado. Até a próxima!")
+        sys.exit()
 
+def main():
+    macas = int(input("Digite a quantidade de maças vendidas: "))
+    bananas = int(input("Digite a quantidade de bananas vendidas: "))
 
-Você conseguiu implementar? Compartilhe com a gente no fórum!
+    if macas > bananas:
+        print(f"\nAs maças tiveram venda {macas} a mais que as bananas {bananas}.")
+    elif bananas > macas:
+        print(f"\nAs bananas tiveram venda {bananas} a mais que as bananas {macas}.")
+    else:
+        print(f"\nHouve empate nas vendas de bananas ({bananas}) e maças ({macas}) .")
 
-"""
+if __name__ == "__main__":
+    os.system('cls' if os.name == 'nt' else 'clear')
+    atividade()
+    main()
