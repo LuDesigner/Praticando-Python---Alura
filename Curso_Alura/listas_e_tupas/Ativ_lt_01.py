1015a8e6c472d9fac59fab7790956aa83119f7f6
LuDesigner <luis.designer.oficial@gmail.com>
2025-09-20 12:47:57 -0300

    Adicionando novas funcionalidades ao projeto

--- Curso_Alura/listas_e_tupas/Ativ_lt_01.py
+++ Curso_Alura/listas_e_tupas/Ativ_lt_01.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import recursos.reinicializar as recursos
 import os
 
@@ -34,40 +33,3 @@ def main():
 if __name__ == "__main__":
     os.system("cls" if os.name == "nt" else "clear")
     main()
-=======
-import recursos.reinicializar as recursos
-import os
-
-def main():
-    os.system("cls" if os.name == "nt" else "clear")
-    print(
-        """
-    Roberto está organizando sua despensa e quer verificar se determinados itens já estão armazenados antes de adicioná-los à lista de compras.
-    Ajude Roberto a criar um programa que pergunte o item desejado e verifique se ele está na lista de itens disponíveis na despensa. 
-    Caso o item não esteja na lista, o programa deve informar que ele precisa ser comprado.
-
-    Exemplo de Entrada: Digite o item que você quer verificar: açúcar
-
-    Saída esperada: O item açúcar precisa ser comprado.
-
-    """
-    )
-
-    despensa = ["arroz", "feijão", "macarrão", "óleo", "sal", "açúcar", "café", "leite"]
-
-    item = input("Digite o item que você quer verificar: ").lower()
-
-    if item in despensa:
-        print(f"O item {item} já está na despensa.")
-    else:
-        print(f"O item {item} precisa ser comprado.")
-
-
-
-    recursos.reinicializar()
-
-
-if __name__ == "__main__":
-    os.system("cls" if os.name == "nt" else "clear")
-    main()
->>>>>>> f25ae57e82f1c2746ed65adf1c7d3f7d140eaa72
